51715143bc02bcf7fc66ea6e846866edeb45c0e6
Matija Osrečki <matija.osrecki@gmail.com>
2022-12-01 13:56:17 +0100

    Add diffenret languages

---
+++ src/lib/components/LanguagePicker.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	export let languages: string[];
+	export let lang = 'en';
+
+	const setLang = (_lang: string) => {
+		lang = _lang;
+	};
+</script>
+
+<div class="text-neutral-50/20 flex items-center justify-end gap-3">
+	{#each languages as $lang}
+		<button on:click={() => setLang($lang)} class:text-neutral-300={lang === $lang}>{$lang}</button>
+	{/each}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,16 @@
 	import Word from '$lib/components/Word.svelte';
 	import type { PageData } from './$types';
 	import Icon from '@iconify/svelte';
+	import LanguagePicker from '$lib/components/LanguagePicker.svelte';
 
 	export let data: PageData;
-	const { wordlist } = data;
+	const { wordlist: wordlists } = data;
+
+	type Lang = keyof typeof wordlists;
+
+	let lang: Lang = 'en';
+	let languages = Object.keys(wordlists).sort() as Lang[];
+	$: wordlist = wordlists[lang];
 
 	let search = '';
 	const clearSearch = () => {
@@ -26,24 +33,28 @@
 	};
 </script>
 
-<header class="flex flex-col sm:flex-row items-start sm:items-center justify-between gap-8">
-	<div>
-		<h1 class="text-2xl">BIP39 Wordlist</h1>
-		<a
-			href="https://halborn.com/what-is-a-bip39/"
-			class="inline-block -ml-1 px-1 mt-2 underline text-sm text-neutral-300/90 decoration-orange-400/50  hover:decoration-orange-400 hover:bg-orange-400/20 hover:opacity-100 rounded-sm"
-		>
-			What is BIP39?
-		</a>
-	</div>
+<header class="">
+	<LanguagePicker bind:lang {languages} />
 
-	<input
-		aria-label="Search word"
-		placeholder="Search"
-		type="search"
-		bind:value={search}
-		class="py-2 px-3 bg-neutral-50/5 focus:ring-1 ring-orange-500 focus:outline-none placeholder:text-neutral-600 rounded-sm w-full sm:w-fit "
-	/>
+	<div class="flex flex-col sm:flex-row items-start justify-between gap-8 mt-6">
+		<div>
+			<h1 class="text-2xl">BIP39 Wordlist</h1>
+			<a
+				href="https://halborn.com/what-is-a-bip39/"
+				class="inline-block -ml-1 px-1 mt-2 underline text-sm text-neutral-300/90 decoration-orange-400/50  hover:decoration-orange-400 hover:bg-orange-400/20 hover:opacity-100 rounded-sm"
+			>
+				What is BIP39?
+			</a>
+		</div>
+
+		<input
+			aria-label="Search word"
+			placeholder="Search"
+			type="search"
+			bind:value={search}
+			class="py-2 px-3 bg-neutral-50/5 focus:ring-1 ring-orange-500 focus:outline-none placeholder:text-neutral-600 rounded-sm w-full sm:w-fit "
+		/>
+	</div>
 </header>
 
 {#if filteredWordlist?.length}
@@ -53,11 +64,11 @@
 		{/each}
 	</main>
 {:else}
-	<div class="mt-20 flex flex-col items-center justify-center gap-5 text-neutral-50/40">
+	<div class="mt-20 flex flex-col items-center justify-center gap-5 text-neutral-50/20">
 		<p class="px-2">No words found</p>
 		<button
 			on:click={clearSearch}
-			class="bg-neutral-50/5 px-2 hover:text-neutral-50/50 focus:ring-2 ring-neutral-600 focus:outline-none flex items-center gap-2"
+			class="bg-neutral-50/5 px-2 hover:text-neutral-50/40 focus:ring-2 ring-neutral-600 focus:outline-none flex items-center gap-2 rounded-sm"
 		>
 			Clear
 			<Icon icon="ic:twotone-clear" />
